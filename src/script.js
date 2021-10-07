3cc29d2d109379261fd7757097882854042ddf9c
Hanka Schmidt <hanka.schmidt@engawa.de>
2021-10-07 12:43:20 +0200

    #improvements

--- src/script.js
+++ src/script.js
@@ -196,6 +196,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
+        
      <div class="col-2">
               <div class="weather-forecast-date">
               ${formatDay(forecastDay.dt)}
